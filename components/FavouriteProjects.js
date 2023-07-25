61f8362f069f2ace7923a10eadfcc895403aea84
Raphael Kipsang <79073634+raphsang@users.noreply.github.com>
2023-07-25 17:44:47 +0300

    Update FavouriteProjects.js

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -93,6 +93,26 @@ export default function FavouriteProjects() {
               </h1>
             </div>
           </a>
+                    {/* Single card */}
+          <a
+            href="https://raphsang.github.io/todoList/"
+            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
+          >
+            <div className="relative top-10 overflow-hidden">
+              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+              <img
+                src="/todolist.png"
+                alt="todo list"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                Todo List Vanilla Javascript 
+              </h1>
+              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                04
+              </h1>
+            </div>
+          </a>
         </div>
       </div>
     </div>
